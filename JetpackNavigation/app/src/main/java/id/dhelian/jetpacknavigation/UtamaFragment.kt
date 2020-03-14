728a894d07386fe98525f8f5a4908359520c27be
dhelianaan <dhelianaan@gmail>
2020-03-14 22:49:28 +0700

    update

--- JetpackNavigation/app/src/main/java/id/dhelian/jetpacknavigation/UtamaFragment.kt
+++ JetpackNavigation/app/src/main/java/id/dhelian/jetpacknavigation/UtamaFragment.kt
@@ -6,11 +6,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_utama.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class UtamaFragment : Fragment() {
+
+    lateinit var nav : NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,5 +25,13 @@ class UtamaFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_utama, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        nav = Navigation.findNavController(view)
+
+        buttonenter.setOnClickListener {
+            nav.navigate(R.id.action_utamaFragment_to_inputFragment)
+        }
+    }
 }
